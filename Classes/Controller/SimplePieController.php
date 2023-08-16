e66b5b2aefb3c483fb77eb4a366fb59d2ef71147
SvenJuergens <SvenJuergens@users.noreply.github.com>
2023-08-16 16:47:58 +0200

    [TASK] some TYPO3 11 and 12 adjustments

--- Classes/Controller/SimplePieController.php
+++ Classes/Controller/SimplePieController.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace SvenJuergens\SimplepieRss\Controller;
 
@@ -14,7 +15,7 @@ namespace SvenJuergens\SimplepieRss\Controller;
  *
  * The TYPO3 project - inspiring people to share!
  */
-
+use Psr\Http\Message\ResponseInterface;
 use SimplePie;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -22,7 +23,7 @@ use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class SimplePieController extends ActionController
 {
-    protected function initializeAction()
+    protected function initializeAction(): void
     {
         if (!is_dir($this->getCacheFolder())) {
             GeneralUtility::mkdir($this->getCacheFolder());
@@ -32,9 +33,9 @@ class SimplePieController extends ActionController
     /**
      * action list
      */
-    public function listAction()
+    public function listAction(): ResponseInterface
     {
-        $feed = new SimplePie();
+        $feed = new SimplePie\SimplePie();
         $feed->enable_cache(true);
         $feed->set_cache_duration(3600);
         $feed->set_cache_location($this->getCacheFolder());
@@ -52,14 +53,15 @@ class SimplePieController extends ActionController
         foreach ($feed->get_items(0, (int)($this->settings['itemLimit']?? 0)) as $item) {
             $markerArray = [
                 'date' => $item->get_local_date('%d.%m.%Y'),
-                'title' => $this->cleanContent(html_entity_decode($item->get_title())) ,
-                'text' => $this->cleanContent($item->get_content()),
+                'title' => $this->cleanContent(html_entity_decode((string)$item->get_title())) ,
+                'text' => $this->cleanContent((string)$item->get_content()),
                 'link' => $item->get_permalink()
             ];
             $items[] = $markerArray;
         }
 
         $this->view->assign('simplePies', $items);
+        return $this->htmlResponse();
     }
 
     private function getCacheFolder(): string
@@ -68,8 +70,11 @@ class SimplePieController extends ActionController
         return  $pathSite . 'typo3temp' . DIRECTORY_SEPARATOR . 'tx_simplepierss' . DIRECTORY_SEPARATOR;
     }
 
-    public function cleanContent($string): string
+    public function cleanContent(string $string = ''): string
     {
+        if (empty($string)) {
+            return $string;
+        }
         //unicode Symbole wie '&#xfc;' ersetzen
         // html_entity_decode holft da nicht
         $replace = [
